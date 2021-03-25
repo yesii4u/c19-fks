02a8789bca5efcdf007aeabc22fabf3d8de4bea9
yesii4u <64878967+yesii4u@users.noreply.github.com>
2021-03-25 16:02:07 +0900

    Initialize

--- components/cards/TestedNumberCard.vue
+++ components/cards/TestedNumberCard.vue
@@ -23,8 +23,8 @@
 <script>
 import Data from '@/data/data.json'
 import formatGraph from '@/utils/formatGraph'
-import TimeBarChart from '@/components/TimeLineChart.vue'
-// import TimeBarChart from '@/components/TimeBarChart.vue'
+// import TimeBarChart from '@/components/TimeLineChart.vue'
+import TimeBarChart from '@/components/TimeBarChart.vue'
 
 export default {
   components: {
